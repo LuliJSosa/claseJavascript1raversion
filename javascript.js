289645dcc7494ddb557f385121d63681ede93dbe
luciasosa <luciajsosa@hotmail.com>
2022-08-30 21:27:36 -0300

    clase javascript emi primera version

---
+++ javascript.js
@@ -0,0 +1,66 @@
+//esto es un alert, es comentario en una linea
+//alert("Hola mundo") 
+
+/*esto
+es comentario
+en multilinea
+*/
+
+
+let alumno = "martin" //creo la variable
+let nota = 9
+
+//const entidad = "Gobierno ciudad"
+// entidad = "Nacion"
+
+console.log( "el nombre del alumno es " + alumno + " y su nota es " + nota ) //la imprimo
+
+//formas de escribir variables
+let nombreApellido = "juan batman"
+let User = "juan carlos"
+let _usuario
+let $user
+let nombre_apellido = "Snake case"
+let NombreApellido 
+
+//no vannnn
+//let 1ernombre //no comienzan con numeros
+//let nombre-NombreApellido //no se ponen signos de operaciones basicas
+//let var super break this //palabras reservadas
+
+
+//tipos de datos
+//STRING
+let str1 = "Juan"   // "String con comillas dobles"
+let str2 = 'Pedro' //'String con comillas simples'
+let str3 = `Raul` //`String con backticks`
+
+//diferentes maneras de sumar
+let str4 = str1 + " y " + str2
+let str5 = `${str1} y ${str2}` 
+console.log (str1 + " y " + str2) 
+console.log (str4)
+console.log (str4)
+
+//NUMBER
+//para que el numero sea dinaico y el user pueda ir cambiando
+let num1 = parseInt (prompt("mete el primer numero")) //parseInt toma num enteros y parseFlout toma num con decimales
+let num2 = parseInt (prompt("mete el segundo numero"))
+//let num1 = 2 
+//let num2 = 3
+
+//operaciones basicas
+let suma = num1 + num2
+
+console.log("La suma es :" + suma)
+console.log (num1 + num2)
+
+let resta = num1 - num2
+console.log("La resta es :" + resta)
+
+let multiplicacion = num1 * num2
+console.log("La multiplicacion es :" + multiplicacion)
+
+let division = num1 / num2
+console.log("La division es :" + division)
+
